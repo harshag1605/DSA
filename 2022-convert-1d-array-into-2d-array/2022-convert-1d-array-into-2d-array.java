9a69ae85ad0b5a77ed38d5d77b39e1b1b88caa3d
Harsh Agrawal <2003harsh16@gmail.com>
2025-09-29 13:42:23 +0530

    Easy

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int matrix[][] = new int[m][n];
+        if(m*n != original.length)return new int[0][0];
+        int k=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j] = original[k++];
+            }
+        }
+        return matrix;
+    }
+}
