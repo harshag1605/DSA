955453b9c9f140f5d73f998c59645e68c403ba37
Harsh Agrawal <2003harsh16@gmail.com>
2025-10-01 14:32:50 +0530

    Hard

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        for(int i=0;i<n;i++){
+            left[i]++;
+            right[i]++;
+        }
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1]+1;
+            }
+        }
+        int count = 0;
+        for(int i=0;i<n;i++){
+            count += Math.max(left[i],right[i]);
+        }
+        return count;
+    }
+}
