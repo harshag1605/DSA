84f4d960ed3d35133569e6f59daef44070059f6d
Harsh Agrawal <2003harsh16@gmail.com>
2025-10-11 23:04:02 +0530

    Easy

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
+            while (!st.isEmpty() && st.peek() <= num) {
+                st.pop();
+            }
+
+            map.put(num, st.isEmpty() ? -1 : st.peek());
+
+            st.push(num);
+        }
+
+        // Build answer for nums1 using the map
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+}
