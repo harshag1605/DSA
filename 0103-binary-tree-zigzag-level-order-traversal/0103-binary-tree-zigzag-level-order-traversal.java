fb09d1a1d8e06d1b7a25f3884adfe17beb46d8ac
Harsh Agrawal <2003harsh16@gmail.com>
2026-03-11 11:19:11 +0530

    Medium

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -17,6 +17,7 @@ class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if(root == null)return res;
+        boolean found = false;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
 
@@ -25,16 +26,20 @@ class Solution {
             List<Integer> list = new ArrayList<>();
             for(int i=0;i<n;i++){
                 TreeNode curr = q.poll();
-                list.add(curr.val);
-                if(curr.left != null)q.add(curr.left);
-                if(curr.right != null)q.add(curr.right);
+                if(found == false){
+                    list.add(curr.val);
+                    if(curr.right != null)q.add(curr.right);
+                    if(curr.left != null)q.add(curr.left);
+                    found = true;
+                }
+                else{
+                    list.add(curr.val);
+                    if(curr.left != null)q.add(curr.left);
+                    if(curr.right != null)q.add(curr.right);
+                    found = false;
+                }
             }
             res.add(list);
-        }
-        for(int i=0;i<res.size();i++){
-            if(i%2 == 1){
-                Collections.reverse(res.get(i));
-            }
         }
         return res;
     }
