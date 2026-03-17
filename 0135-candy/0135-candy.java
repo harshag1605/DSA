35d12c724994760b451a2d7a734d55d6bddec9f1
Harsh Agrawal <2003harsh16@gmail.com>
2026-03-17 11:29:21 +0530

    Hard

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,23 +2,22 @@ class Solution {
     public int candy(int[] rating) {
         int n = rating.length;
         if(n == 1)return 1;
-        // int res[] = new int[n];
         int[] pre = new int[n];
         int[] suf = new int[n];
-        // Arrays.fill(res,1);
-        for(int i=0;i<n-1;i++){
-            if(rating[i] > rating[i+1]){
-                suf[i]++;
-            }
-        }
-        for(int i=n-1;i>0;i--){
+        Arrays.fill(pre,1);
+        Arrays.fill(suf,1);
+        for(int i=1;i<n;i++){
             if(rating[i] > rating[i-1]){
-                pre[i]++;
+                pre[i] = pre[i-1]+1;
             }
         }
-        int sum = n;
+        for(int i=n-2;i>=0;i--){
+            if(rating[i] > rating[i+1]){
+                suf[i] = suf[i+1]+1;
+            }
+        }
+        int sum = 0;
         for(int i=0;i<n;i++){
-            // sum += res[i];
             sum += Math.max(pre[i],suf[i]);
         }
         return sum;
