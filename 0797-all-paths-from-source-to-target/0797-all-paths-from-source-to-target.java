b4360708a2bb71c84479c4718fb40d4fdc7ee984
Harsh Agrawal <2003harsh16@gmail.com>
2026-03-19 12:28:44 +0530

    Medium

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,17 +1,35 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> res = new ArrayList<>();
-        dfs(0,graph,new ArrayList<>(),res);
-        return res;
-    }
-    void dfs(int src,int[][] g,List<Integer> path,List<List<Integer>> ans){
+        List<List<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        int n = graph.length;
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                adj.get(i).add(graph[i][j]);
+            }
+        }
+        int src = 0;
+        int dest = n-1;
         path.add(src);
-        if(src == g.length-1){
+        dfs(src,adj,path,ans,dest);
+
+        return ans;
+    }
+
+    void dfs(int src,List<List<Integer>> adj,List<Integer> path,List<List<Integer>> ans,int dest){
+        if(src == dest){
             ans.add(new ArrayList<>(path));
+            return;
         }
-        for(int neigh : g[src]){
-            dfs(neigh,g,path,ans);
+        for(int neigh : adj.get(src)){
+            path.add(neigh);
+            dfs(neigh,adj,path,ans,dest);
+            path.remove(path.size()-1);
         }
-        path.remove(path.size()-1);
     }
 }
