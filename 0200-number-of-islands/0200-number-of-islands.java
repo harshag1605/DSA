4d1df4c0cf3052ae57dded0e83ee79a5d2566656
Harsh Agrawal <2003harsh16@gmail.com>
2026-03-24 21:11:48 +0530

    Medium

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,24 +3,26 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int count = 0;
+        boolean[][] vis = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    dfs(grid,i,j,vis);
                     count++;
-                    dfs(i,j,grid);
                 }
             }
         }
         return count;
     }
-    static void dfs(int i,int j,char[][] grid){
-        if(i<0 || j<0 || i>grid.length-1 || j>grid[0].length-1 || grid[i][j]=='0'){
+    void dfs(char[][] grid,int i,int j,boolean[][] vis){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0' || vis[i][j]){
             return;
         }
-        grid[i][j] = '0'; // mark
-        dfs(i+1,j,grid);
-        dfs(i,j+1,grid);
-        dfs(i-1,j,grid);
-        dfs(i,j-1,grid);
+        
+        vis[i][j] = true;
+        dfs(grid,i+1,j,vis);
+        dfs(grid,i-1,j,vis);
+        dfs(grid,i,j+1,vis);
+        dfs(grid,i,j-1,vis);
     }
 }
