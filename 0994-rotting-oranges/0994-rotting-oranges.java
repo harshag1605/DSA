a8ede88468f98d72ef0103ddb1ac0c34b24f5f08
Harsh Agrawal <2003harsh16@gmail.com>
2026-04-01 23:35:00 +0530

    Medium

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,34 +1,38 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
+        int n = grid.length;
+        int m = grid[0].length;
+        int time = 0;
         int fresh = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 2){
-                    q.add(new int[]{i,j});
-                }
-                else if(grid[i][j] == 1)fresh++;
+        Queue<int[]> q = new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 2)q.add(new int[]{i,j});
+                if(grid[i][j] == 1)fresh++;
             }
         }
-        if(fresh == 0)return 0;
-        int m = 0;
-        int dir[][] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.isEmpty()){
-            m++;
             int size = q.size();
-            while(size-- > 0){
-                int f[] = q.poll();
-                for(int d[] : dir){
-                    int x = f[0]+d[0];
-                    int y = f[1]+d[1];
-                    if(x<0 || x>=grid.length || y<0 || y>=grid[0].length || grid[x][y]==2 || grid[x][y]==0)continue;
-                    fresh--;
-                    grid[x][y] = 2;
-                    if(fresh == 0)return m;
-                    q.add(new int[]{x,y});
+            boolean rotted = false;
+            for(int i=0;i<size;i++){
+                int[] curr = q.poll();
+                for(int[] d : dir){
+                    int ni = curr[0]+d[0];
+                    int nj = curr[1]+d[1];
+                    if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1){
+                        grid[ni][nj] = 2;
+                        fresh--;
+                        q.add(new int[]{ni,nj});
+                        rotted = true;
+                    }
                 }
             }
+            if(rotted)time++;
         }
-        return -1;
+        if(fresh > 0)return -1;
+        return time;
     }
 }
