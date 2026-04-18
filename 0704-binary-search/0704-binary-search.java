60ab758e1816517106c6cae1805e2918fb850e59
Harsh Agrawal <2003harsh16@gmail.com>
2026-04-18 17:08:51 +0530

    Easy

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return helper(nums,target,0,nums.length-1);
+    }
+    int helper(int nums[], int target, int st, int end){
+        if(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] == target)return mid;
+            else if(nums[mid] < target){
+                return helper(nums,target,mid+1,end);
+            }
+            else{
+                return helper(nums,target,st,mid-1);
+            }
+        }
+        return -1;
+    }
+}
