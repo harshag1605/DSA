547321022f7a55d8185eeac50325961543935739
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-07 23:17:28 +0530

    Medium

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int n = trips.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(trips[i][1] < min){
+                min = trips[i][1];
+            }
+            if(trips[i][2] > max){
+                max = trips[i][2];
+            }
+        }
+        int[] arr = new int[max+1];
+        for(int i=0;i<n;i++){
+            int x = trips[i][1];
+            int y = trips[i][2];
+            int z = trips[i][0];
+            for(int j=x;j<y;j++){
+                arr[j] += z;
+                if(arr[j] > capacity) return false;
+            }
+        }
+        return true;
+    }
+}
