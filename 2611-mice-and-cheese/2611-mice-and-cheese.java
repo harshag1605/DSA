cd91dd52f133342ad3d3fb7d03f90e70201d03d3
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-07 14:42:42 +0530

    Medium

--- 2611-mice-and-cheese/2611-mice-and-cheese.java
+++ 2611-mice-and-cheese/2611-mice-and-cheese.java
@@ -2,14 +2,14 @@ class Solution {
     public int miceAndCheese(int[] reward1, int[] reward2, int k) {
         int n = reward2.length;
         int sum = 0;
-        for(int i=0;i<n;i++) sum += reward2[i];
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> (b-a));
+        int[] diff = new int[n];
         for(int i=0;i<n;i++){
-            pq.add(reward1[i]-reward2[i]);
+            diff[i] = reward1[i]-reward2[i];
+            sum += reward2[i];
         }
+        Arrays.sort(diff);
         for(int i=0;i<k;i++){
-            int x = pq.poll();
-            sum += x;
+            sum += diff[n-1-i];
         }
         return sum;
     }
