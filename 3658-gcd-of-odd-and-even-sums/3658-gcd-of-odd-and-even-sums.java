8a3f8b63b840c3b1fabd260cd438c7c3e13f8bb5
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-15 23:39:46 +0530

    Easy

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int odd = 0;
+        int even = 0;
+        for(int i=1;i<=n*2;i++){
+            if(i%2 == 0) even += i;
+            else odd += i;
+        }
+        int min = Math.min(odd,even);
+        int max = Integer.MIN_VALUE;
+        for(int i=1;i<=min;i++){
+            if(odd%i == 0 && even%i == 0){
+                max = Math.max(i,max);
+            }
+        }
+        return max;
+    }
+}
