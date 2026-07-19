009ecc34df263fa99dd76800430de4297b346ef4
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-19 22:42:21 +0530

    Medium

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int n = s.length();
+        int[] freq = new int[26];
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            int idx = (int)ch-'a';
+            freq[idx]++;
+        }
+        boolean[] flag = new boolean[26];
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            freq[ch-'a']--;
+            if(flag[ch-'a']) continue;
+            while(!st.isEmpty() && st.peek() > ch && freq[st.peek()-'a'] > 0){
+                flag[st.pop()-'a'] = false;
+            }
+            st.push(ch);
+            flag[ch-'a'] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
