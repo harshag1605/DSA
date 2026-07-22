94ff2e420c99a1d0ce67e695db147f53a56134e4
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-22 12:35:52 +0530

    Medium

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,21 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int tank = 0;
-        int totalgas = 0;
-        int totalcost = 0;
-        int start = 0;
-        for(int i=0;i<gas.length;i++){
-            totalgas += gas[i];
-            totalcost += cost[i];
-            tank += gas[i]-cost[i];
-            if(tank < 0){
-                start = i+1;
-                tank=0;
+        int n = gas.length;
+        int sumg = 0;
+        int sumc = 0;
+        for(int i=0;i<n;i++){
+            sumg += gas[i];
+            sumc += cost[i];
+        }
+        if(sumg < sumc) return -1;
+
+        int cf = 0;
+        int st = 0;
+        for(int i=0;i<n;i++){
+            cf += gas[i]-cost[i];
+            if(cf < 0){
+                st = i+1;
+                cf = 0;
             }
         }
-        if(totalgas >= totalcost)return start;
-        else{
-            return -1;
-        }
+        return st;
     }
 }
