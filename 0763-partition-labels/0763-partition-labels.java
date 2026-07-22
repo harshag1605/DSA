6880bad71c1743a899eaeeee9663d617cf22fe09
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-22 16:57:06 +0530

    Medium

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -7,20 +7,16 @@ class Solution {
             char ch = s.charAt(i);
             map.put(ch,i);
         }
-        int count = 0;
-        int max = map.get(s.charAt(0));
-        for(int i=1;i<n;i++){
+        int start = 0;
+        int max = 0;
+        for(int i=0;i<n;i++){
             char ch = s.charAt(i);
-            if(max >= i){
-                max = Math.max(max,map.get(ch));
-            }
-            else{
-                max = Math.max(max,map.get(ch));
-                res.add(i-count);
-                count = i;
+            max = Math.max(max, map.get(ch));
+            if(i==max){
+                res.add(i-start+1);
+                start = i+1;
             }
         }
-        res.add(n-count);
         return res;
     }
 }
