a8945627097eab607f3c7b3a01c33dd279db6c3d
Harsh Agrawal <2003harsh16@gmail.com>
2026-07-22 13:22:18 +0530

    Medium

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0;
-    int currentEnd = 0;
-    int farthest = 0;
-
-    for (int i = 0; i < nums.length - 1; i++) {
-        farthest = Math.max(farthest, i + nums[i]);
-
-        if (i == currentEnd) {
-            jumps++;
-            currentEnd = farthest;
+        int n = nums.length;
+        int steps = 0;
+        int reach = 0;
+        int curr = 0;
+        for(int i=0;i<n-1;i++){
+            reach = Math.max(reach, i+nums[i]);
+            
+            if(i==curr){
+                steps++;
+                curr = reach;
+            }
         }
-    }
-    return jumps;
+        return steps;
     }
 }
