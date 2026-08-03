a87915d6740e51718ebc64122c52fe27682ba978
Harsh Agrawal <2003harsh16@gmail.com>
2026-08-03 23:47:33 +0530

    Medium

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
         Arrays.sort(nums);
-        helper(res,temp,0,nums);
-        return res;
-
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        helper(nums,ans,temp,0);
+        return ans;   
     }
-    void helper(List<List<Integer>> res, List<Integer> temp, int i, int[] nums){
-        if(i == nums.length){
-            res.add(new ArrayList<>(temp));
+    public void helper(int[] nums, List<List<Integer>> ans, List<Integer> temp, int idx){
+        if(idx == nums.length){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        //include
-        temp.add(nums[i]);
-        helper(res,temp,i+1,nums);
+        temp.add(nums[idx]);
+        helper(nums,ans,temp,idx+1);
 
-        int idx = i+1;
-        //exclude
         temp.remove(temp.size()-1);
-        while(idx < nums.length && nums[idx] == nums[idx-1]) idx++;
-        helper(res,temp,idx,nums);
+        while(idx+1<nums.length && nums[idx] == nums[idx+1]){
+            idx++;
+        }
+        helper(nums,ans,temp,idx+1);
     }
 }
