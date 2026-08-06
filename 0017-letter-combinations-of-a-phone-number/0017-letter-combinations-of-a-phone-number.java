ed19ebcb2bcbd9636fb2605fc31fd558cf870638
Harsh Agrawal <2003harsh16@gmail.com>
2026-08-06 23:17:37 +0530

    Medium

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,22 +2,19 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
         String[] s = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        StringBuilder sb = new StringBuilder();
-        helper(digits,ans,s,sb,0);
+        String st = "";
+        helper(digits,ans,s,st,0);
         return ans;
     }
-    public void helper(String digits, List<String> ans, String[] s, StringBuilder sb, int idx){
+    public void helper(String digits, List<String> ans, String[] s, String st, int idx){
         if(idx == digits.length()){
-            ans.add(sb.toString());
+            ans.add(st);
             return;
         }
         int val = digits.charAt(idx)-'0';
         String map = s[val];
         for(int i=0;i<map.length();i++){
-            sb.append(map.charAt(i));
-            helper(digits,ans,s,sb,idx+1);
-
-            sb.deleteCharAt(sb.length()-1);
+            helper(digits,ans,s,st+map.charAt(i),idx+1);
         }
     }
 }
