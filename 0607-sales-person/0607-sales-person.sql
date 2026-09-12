45e0cb38eff19bbeb2cfc03b1d53b807857d5b03
Harsh Agrawal <2003harsh16@gmail.com>
2026-09-12 12:13:16 +0530

    Easy

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select s.name
+from SalesPerson s
+where s.sales_id not in (select o.sales_id 
+from Orders o
+left join Company c
+on o.com_id = c.com_id
+where o.sales_id = s.sales_id and c.name = "RED");
