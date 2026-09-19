ed6ab11ef3d0472fcd3ea6337cecf0376ab5cbab
Harsh Agrawal <2003harsh16@gmail.com>
2026-09-19 18:18:49 +0530

    Medium

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,6 @@
-SELECT MAX(salary) AS SecondHighestSalary
-FROM (
-    SELECT 
-        salary,
-        DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-    FROM Employee
-) as t
-WHERE rnk = 2;
+SELECT (
+    SELECT DISTINCT salary 
+    FROM Employee 
+    ORDER BY salary DESC 
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
